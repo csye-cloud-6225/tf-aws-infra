4d6e3df6c63888f5e862741490a0748f988a147d
Nilvi Shah <144835276+nilvishah@users.noreply.github.com>
2024-10-16 16:52:50 -0400

    terraform demo

--- main.tf
+++ main.tf
@@ -4,7 +4,8 @@ provider "aws" {
 }
 # VPC
 resource "aws_vpc" "main_vpc" {
-  cidr_block           = var.vpc_cidr
+
+    cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
