155da159c8e879d5b9fb570fd0c84bb8e237cbe3
Nilvi Shah <144835276+nilvishah@users.noreply.github.com>
2024-10-16 16:55:37 -0400

    demo3-3

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 # VPC
 resource "aws_vpc" "main_vpc" {
-  cidr_block           = varr.vpc_cidr
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
